da2d269d233a2a1681d13335955c7d2032657b59
Morgan Jacob <jacobmmori@gmail.com>
2024-09-08 17:11:40 -0400

    Create determinePerformanceRating function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,11 +1,12 @@
 // Create a Function to Calculate Average Sales
 
+
 let calculateAverageSales = (sales) => {
     let salesSum = sales.reduce((sum, figure) => sum + figure,0 );
     let averageSales = salesSum / sales.length;
     return averageSales
 }
-console.log(calculateAverageSales);
+console.log(`Average sales: $${calculateAverageSales(sales)}`);
 
 // Create a Function to Determine Performance Rating
 
